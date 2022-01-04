94c33c862f2173738172359ad2ef5ce6f6c4135f
dvauio <davidvaughan92@outlook.com>
2022-01-04 22:25:38 +0000

    Create UbuntuServerInitialConfig.sh

---
+++ UbuntuServerInitialConfig.sh
@@ -0,0 +1,10 @@
+#Update OS
+sudo apt update && sudo apt upgrrade -y
+
+# Apt over HTTPS
+sudo apt install apt-transport-https gnupg1 dirmngr
+
+#Install & Enable Fail2ban
+sudo apt install fail2ban
+sudo systemctl status fail2ban
+sudo cp /etc/fail2ban/jail.{conf,local}
