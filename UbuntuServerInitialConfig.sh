fab11db3413a3622c370a85749ffe382ced23107
dvauio <davidvaughan92@outlook.com>
2022-01-04 23:37:13 +0000

    Update UbuntuServerInitialConfig.sh

--- UbuntuServerInitialConfig.sh
+++ UbuntuServerInitialConfig.sh
@@ -1,5 +1,7 @@
 # One line run
-# curl https://raw.githubusercontent.com/dvauio/Speedtest/main/UbuntuServerInitialConfig.sh -o UbuntuServerInitialConfig.sh && chmod +x UbuntuServerInitialConfig.sh |bash UbuntuServerInitialConfig.sh
+# curl https://raw.githubusercontent.com/dvauio/Speedtest/main/UbuntuServerInitialConfig.sh|bash
+
+cd /home/day/Apps/Speedtest
 
 # Update OS
 sudo apt install software-properties-common -y
@@ -9,7 +11,6 @@ sudo apt update && sudo apt upgrrade -y
 sudo apt install apt-transport-https gnupg1 dirmngr -y
 
 #install Python3
-sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3 -y
 
 # Install & Enable Fail2ban
@@ -17,12 +18,11 @@ sudo apt install fail2ban -y
 sudo systemctl status fail2ban
 sudo cp /etc/fail2ban/jail.{conf,local}
 
-#Install Speedtest CLI
+# Install Speedtest CLI
 curl -L https://packagecloud.io/ookla/speedtest-cli/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/speedtestcli-archive-keyring.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/speedtestcli-archive-keyring.gpg] https://packagecloud.io/ookla/speedtest-cli/debian/ $(lsb_release -cs) main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
 sudo apt update
 sudo apt install speedtest -y
-#speedtest
 
 # Make folder for Script
 sudo mkdir -m 777 /opt/speedtest
